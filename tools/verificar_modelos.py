596423c7c6f0bec991dd6dd6e0d9677b2639fec7
Mateus Lima <mateuslimasantos909@gmail.com>
2026-08-07 13:10:47 +0000

    refactor: atualizção de script de verificação de modelo

--- tools/verificar_modelos.py
+++ tools/verificar_modelos.py
@@ -1,16 +1,16 @@
-import google.generativeai as genai
+from google import genai
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv(Path(".") / ".env")
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-print("🔍 Perguntando ao Google quais modelos estão disponíveis para você...")
+print("Perguntando ao Google quais modelos estão disponíveis...")
 
 try:
-    for m in genai.list_models():
-        if 'generateContent' in m.supported_generation_methods:
+    for m in client.models.list():
+        if 'generateContent' in m.supported_actions:
             print(f"✅ Disponível: {m.name}")
 except Exception as e:
     print(f"❌ Erro ao listar: {e}")
